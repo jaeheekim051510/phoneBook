1826a2fcedd81cedf95df0f2bd88b6385a5763d4
Jaehee Kim <jaeheekim051510@gmail.com>
2018-03-20 02:13:37 -0400

    save and list all entry option added

--- phone.js
+++ phone.js
@@ -5,10 +5,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-var aData = [
-    {"firstName":"JAEHEE", "lastName":"KIM", "phoneNumber":"619-222-2222"},
-    {"firstName":"MARK", "lastName":"SMITH", "phoneNumber":"619-111-1112"}
-];
+var aData = [];
 
 
 var newData = function(firstName, lastName, phoneNumber) {
@@ -18,6 +15,7 @@ var newData = function(firstName, lastName, phoneNumber) {
     newEntry.phoneNumber = phoneNumber;
     aData.push(newEntry);
 }
+
 
 var question = function() {
     rl.question("=====================\n1.Look up and entry\n2.Set an entry\n3.Delete an entry\n4.List all entries\n5.Quit\n=====================\nWhat do you want to to (1-5)? ", function (answer) {
@@ -31,12 +29,13 @@ var question = function() {
             console.log('3');
         }
         else if (answer === "4") {
-            listAllEntry();
+            listData();
         }
         else if (answer === "5") {
-            addData(aData);
+            console.log('Thank you!');
             rl.close();
         }
+
         else {
             console.log('please type corret entry.')
             question();
@@ -67,11 +66,32 @@ var setEntry = function() {
         var lastName = lastName.toUpperCase();
         rl.question("phoneNumber(000-000-0000): ", function(phoneNumber){
             newData(firstName, lastName, phoneNumber);
+            saveData();
             console.log(`${firstName} ${lastName}'s phone number has been saved.`);
             question();
         });
     });
 });
+};
+
+var saveData = function() {
+    fs.writeFile('contact.txt', JSON.stringify(aData), function(err) {
+        if (err) {
+        console.log('err');
+        };
+    });
+};
+
+var listData = function() {
+    fs.readFile('contact.txt', 'utf8', function(err, fileContents){
+        if (err) {
+            console.log('error')
+        }
+        else {
+            console.log(fileContents);
+            question();
+            }
+    });
 };
 
 var listAllEntry = function () {
@@ -82,4 +102,3 @@ var listAllEntry = function () {
 }
 
 question();
-
